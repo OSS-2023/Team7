3cf2d9a3f0f7e72fdd9dcaab32aa0dd2d060b294
SangYoonLee <prestro1231@hufs.ac.kr>
2023-06-30 15:08:59 +0900

    complex_7 파일 추가

---
+++ complex_7.py
@@ -0,0 +1,32 @@
+class Complex:
+    def __init__(self, re=0, im=0):
+        self.re = re
+        self.im = im
+
+    def __str__(self):
+        if self.im < 0:
+            return str(self.re) + '-' + str(-self.im)+ 'i'
+        else:
+            return str(self.re) + '-' + str(-self.im) + 'i'
+
+    # member1
+    def __add__(self, c): # return self+c
+        return self+c
+
+    # member2
+    def __sub__(self, c): # return self-c
+        pass
+
+    # member3
+    def __mul__(self, c): # return self*c
+        pass
+
+    # member4
+    # print("member4")
+
+    
+c1 = Complex(1,2)
+c2 = Complex(2,-2)
+print(c1+c2)
+print(c1-c2)
+print(c1*c2)
